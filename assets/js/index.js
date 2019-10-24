c35737c5fce58eb13de9d03f22b4650773fe6114
AS <albrecht.stephanie@outlook.fr>
2019-10-24 12:17:06 +0200

    correction

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,10 @@
-var prenom = prompt("Entrer votre prénom");
-if (prenom.trim() != ("")){
+var prenom = prompt("Entrer votre prénom");//boite de dial et récupération de la donnée
+if (prenom.trim() != ("")){ //verifie que le champ n'est pas vide
   alert(`Bonjour, ${prenom}`);
 }
 else{
   alert('Veuillez saisir quelque chose.')
 }
+/* while(lastName == ''){
+  lastName = windows.prompt('entrer votre prénom');
+}*///bonus
